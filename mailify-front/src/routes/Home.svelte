75488bbf4ee5f4d58367e74c267aaf101f423e6e
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-12-13 22:26:46 +0700

    feat: three js for canvas

--- mailify-front/src/routes/Home.svelte
+++ mailify-front/src/routes/Home.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Navbar from "../components/Navbar.svelte";
     import Footer from "../components/Footer.svelte";
-    import ComputerScene from "../components/interactivity/ComputerScene.svelte";
+    import ComputerScene from "../components/background/HomeBackground.svelte";
 </script>
 
 <div>
