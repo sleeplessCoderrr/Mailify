6eb4ddfca1d6dac52bbc3b7d4fcf561ca2b92c15
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-12-16 09:33:47 +0700

    feat: i word in write for hero

--- mailify-front/src/routes/Home.svelte
+++ mailify-front/src/routes/Home.svelte
@@ -2,6 +2,8 @@
     import Navbar from "../components/Navbar.svelte";
     import Footer from "../components/Footer.svelte";
     import HomeBackground from "../components/background/HomeBackground.svelte";
+    import MailLogo from "../components/essentials/icon/MailLogo.svelte";
+    import Write from "../components/essentials/Write.svelte";
 </script>
 
 <div class="flex-col justify-items-center items-center w-full">
@@ -11,13 +13,12 @@
 
         <div class="flex-column justify-items-center">
             <div class="flex flex-row gap-4 self-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 self-center bg-blaze rounded p-0 stroke-stone-200">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                </svg>
-                <p class="text-base my-4 font-medium">KNOW EVERY PARAMETER IN EASY REACH</p>
+                <MailLogo/>
+                <p class="text-base my-4 font-medium">TRANSFORM YOUR EMAILS, UNLOCK OPPORTUNITIES</p>
             </div>
-            <p class="text-8xl  my-6 font-semibold">Control Your Car</p>
-            <p class="text-8xl  my-6 font-medium">Via Your Phone</p>
+            <p class="text-8xl my-6 font-semibold text-center">
+                <Write/> Email <br/> That Get Noticed
+            </p>
         </div>
 
         <Footer/>
